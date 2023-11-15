a249b289cfd86fdaf5d49e1c9ba7b742838b7031
Carlos Henrique <carloshenriquepereirasoaress@gmail.com>
2023-11-15 16:13:22 -0300

    Domain model

--- src/main/java/com/devsuperior/dslist/entities/GameList.java
+++ src/main/java/com/devsuperior/dslist/entities/GameList.java
@@ -10,7 +10,7 @@ public class GameList {
 
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY
     private Long id;
     private String name;
 
