4f15bded74a37cfe348a4eb90274ac3791a53bbe
Carlos Henrique <carloshenriquepereirasoaress@gmail.com>
2023-11-15 13:27:22 -0300

    Project created

---
+++ src/test/java/com/devsuperior/dslist/IntensivaoJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntensivaoJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
