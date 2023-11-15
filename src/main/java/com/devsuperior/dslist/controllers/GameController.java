5bd175eea64dcd0d2aec8460b8107a5c8c2a4136
Carlos Henrique <carloshenriquepereirasoaress@gmail.com>
2023-11-15 15:41:13 -0300

    Class refactoring GameController

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -1,10 +1,8 @@
 package com.devsuperior.dslist.controllers;
 
 import com.devsuperior.dslist.dto.GameMinDTO;
-import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
